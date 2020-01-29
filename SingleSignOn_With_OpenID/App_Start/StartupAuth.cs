28fc8dca7ca5654a29b0890445b903bba23b571f
Ramesh Selvaraj <itramji3508@gmail.com>
2020-01-29 10:39:20 +0530

    Committed

--- SingleSignOn_With_OpenID/App_Start/StartupAuth.cs
+++ SingleSignOn_With_OpenID/App_Start/StartupAuth.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Owin.Extensions;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.Owin.Extensions;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Notifications;
 using Microsoft.Owin.Security.OpenIdConnect;
 using Owin;
 
@@ -12,7 +16,7 @@ namespace SingleSignOn_With_OpenID
 		private const string strClientId = "9aabbb3a-6a68-44fa-a143-abefc7f299c2";
 		private const string strInstance = "https://login.microsoftonline.com/";
 		private const string strTenantId = "8b67b292-ebf3-4d29-89a6-47f7971c2e16";
-		private const string strRedirectUri = "https://localhost:44363/";
+		private const string strRedirectUri = "https://localhost:44363/About.aspx";
 		#endregion
 
 		#region Fields
@@ -31,10 +35,38 @@ namespace SingleSignOn_With_OpenID
 			                                   {
 				                                   ClientId = strClientId,
 				                                   Authority = strAuthority,
-				                                   PostLogoutRedirectUri = strRedirectUri
+				                                   PostLogoutRedirectUri = strRedirectUri,
+				                                   Scope = "openid",
+				                                   //ResponseType = "id_token",
+				                                   RedirectUri = strRedirectUri,
+												   //MetadataAddress = "https://login.microsoftonline.com/8b67b292-ebf3-4d29-89a6-47f7971c2e16/v2.0/.well-known/openid-configuration",
+												   Notifications = new OpenIdConnectAuthenticationNotifications
+				                                                   {
+																	   SecurityTokenReceived = OnSecurityTokenReceived,
+																	   AuthorizationCodeReceived = OnAuthorizationCodeReceived,
+					                                                   AuthenticationFailed = OnAuthenticationFailed
+				                                                   }
 			                                   }
 			                                  );
 			app.UseStageMarker(PipelineStage.Authenticate);
+		}
+		#endregion
+
+		#region Private
+		private Task OnAuthorizationCodeReceived(AuthorizationCodeReceivedNotification arg)
+		{
+			AuthenticationTicket authTicket = arg.AuthenticationTicket;
+
+			return Task.FromResult(0);
+		}
+		private Task OnAuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+		{
+			return Task.FromResult(0);
+		}
+
+		private Task OnSecurityTokenReceived(SecurityTokenReceivedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> arg)
+		{
+			return Task.FromResult(0);
 		}
 		#endregion
 	}
